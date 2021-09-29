8aefe82782314cd190161483f1f97499acfd728b
acem0301 <acem0301@gmail.com>
2021-09-28 22:52:42 -0400

    Se agrega condicion en listado

--- src/pages/search-list.js
+++ src/pages/search-list.js
@@ -154,108 +154,111 @@ const SearchGrid = () => {
                     </div>
                   </div>
                 </form>
-                <div className="pt-12">
-                  <div className="d-flex align-items-center justify-content-between mb-6">
-                    <h5 className="font-size-4 font-weight-normal text-gray">
-                      <span className="heading-default-color">120 </span>
-                      resultados para{" "}
-                      <span className="heading-default-color">Manicurista</span>
-                    </h5>
-                    <div className="d-flex align-items-center result-view-type">
-                      <Link href="/search-list">
-                        <a className="heading-default-color pl-5 font-size-6 hover-text-hitgray active">
-                          <i className="fa fa-list-ul"></i>
-                        </a>
-                      </Link>
-                      <Link href="/search-grid">
-                        <a className="heading-default-color pl-5 font-size-6 hover-text-hitgray">
-                          <i className="fa fa-th-large"></i>
-                        </a>
-                      </Link>
+                {dataResult != null && (
+                  <div className="pt-12">
+                    <div className="d-flex align-items-center justify-content-between mb-6">
+                      <h5 className="font-size-4 font-weight-normal text-gray">
+                        <span className="heading-default-color">120 </span>
+                        resultados para{" "}
+                        <span className="heading-default-color">
+                          Manicurista
+                        </span>
+                      </h5>
+                      <div className="d-flex align-items-center result-view-type">
+                        <Link href="/search-list">
+                          <a className="heading-default-color pl-5 font-size-6 hover-text-hitgray active">
+                            <i className="fa fa-list-ul"></i>
+                          </a>
+                        </Link>
+                        <Link href="/search-grid">
+                          <a className="heading-default-color pl-5 font-size-6 hover-text-hitgray">
+                            <i className="fa fa-th-large"></i>
+                          </a>
+                        </Link>
+                      </div>
                     </div>
-                  </div>
 
-                  {dataResult.map((profesional) => {
-                    return (
-                      <div className="mb-8">
-                        <div className="pt-9 px-xl-9 px-lg-7 px-7 pb-7 light-mode-texts bg-white rounded hover-shadow-3 ">
-                          <div className="row">
-                            <div className="col-md-6">
-                              <div className="media align-items-center">
-                                <div className="square-72 d-block mr-8">
-                                  <img src={imgF1} alt="" />
-                                </div>
-                                <div>
-                                  <h3 className="mb-0">
+                    {dataResult.map((profesional) => {
+                      return (
+                        <div className="mb-8">
+                          <div className="pt-9 px-xl-9 px-lg-7 px-7 pb-7 light-mode-texts bg-white rounded hover-shadow-3 ">
+                            <div className="row">
+                              <div className="col-md-6">
+                                <div className="media align-items-center">
+                                  <div className="square-72 d-block mr-8">
+                                    <img src={imgF1} alt="" />
+                                  </div>
+                                  <div>
+                                    <h3 className="mb-0">
+                                      <Link href="/#">
+                                        <a className="font-size-6 heading-default-color">
+                                          {profesional.persona.nombre}{" "}
+                                          {profesional.persona.apellido}
+                                        </a>
+                                      </Link>
+                                    </h3>
                                     <Link href="/#">
-                                      <a className="font-size-6 heading-default-color">
-                                        {profesional.persona.nombre}{" "}
-                                        {profesional.persona.apellido}
+                                      <a className="font-size-3 text-default-color line-height-2">
+                                        {profesional.nombre}
                                       </a>
                                     </Link>
-                                  </h3>
-                                  <Link href="/#">
-                                    <a className="font-size-3 text-default-color line-height-2">
-                                      {profesional.nombre}
-                                    </a>
-                                  </Link>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="col-md-6 text-right pt-7 pt-md-5">
+                                <div className="media justify-content-md-end">
+                                  <div className="image mr-5 mt-2">
+                                    <img src={imgF} alt="" />
+                                  </div>
+                                  <p className="font-weight-bold font-size-7 text-hit-gray mb-0">
+                                    <span className="text-black-2">
+                                      {
+                                        profesional.servicio_profesional[0]
+                                          .servicio.monto_hora
+                                      }
+                                    </span>{" "}
+                                    Gs.
+                                  </p>
                                 </div>
                               </div>
                             </div>
-                            <div className="col-md-6 text-right pt-7 pt-md-5">
-                              <div className="media justify-content-md-end">
-                                <div className="image mr-5 mt-2">
-                                  <img src={imgF} alt="" />
-                                </div>
-                                <p className="font-weight-bold font-size-7 text-hit-gray mb-0">
-                                  <span className="text-black-2">
-                                    {
-                                      profesional.servicio_profesional[0]
-                                        .servicio.monto_hora
+                            <div className="row pt-8">
+                              <div className="col-md-7">
+                                <ul className="d-flex list-unstyled mr-n3 flex-wrap">
+                                  {profesional.servicio_profesional.map(
+                                    (servicio) => {
+                                      return (
+                                        <li>
+                                          <Link href="">
+                                            <a className="bg-regent-opacity-15 min-width-px-96 mr-3 text-center rounded-3 px-6 py-1 font-size-3 text-black-2 mt-2">
+                                              {servicio.servicio.descripcion}
+                                            </a>
+                                          </Link>
+                                        </li>
+                                      );
                                     }
-                                  </span>{" "}
-                                  Gs.
-                                </p>
+                                  )}
+                                </ul>
                               </div>
-                            </div>
-                          </div>
-                          <div className="row pt-8">
-                            <div className="col-md-7">
-                              <ul className="d-flex list-unstyled mr-n3 flex-wrap">
-                                {profesional.servicio_profesional.map(
-                                  (servicio) => {
-                                    return (
-                                      <li>
-                                        <Link href="">
-                                          <a className="bg-regent-opacity-15 min-width-px-96 mr-3 text-center rounded-3 px-6 py-1 font-size-3 text-black-2 mt-2">
-                                            {servicio.servicio.descripcion}
-                                          </a>
-                                        </Link>
-                                      </li>
-                                    );
-                                  }
-                                )}
-                              </ul>
-                            </div>
-                            <div className="col-md-5">
-                              <ul className="d-flex list-unstyled mr-n3 flex-wrap mr-n8 justify-content-md-end">
-                                <li className="mt-2 mr-8 font-size-small text-black-2 d-flex">
-                                  <span
-                                    className="mr-4"
-                                    css={`
-                                      margin-top: -2px;
-                                    `}
-                                  >
-                                    <img src={iconL} alt="" />
-                                  </span>
-                                  <span className="font-weight-semibold">
-                                    {
-                                      profesional.persona.persona_direccion[0]
-                                        .ciudad.nombre
-                                    }
-                                  </span>
-                                </li>
-                                {/* <li className="mt-2 mr-8 font-size-small text-black-2 d-flex">
+                              <div className="col-md-5">
+                                <ul className="d-flex list-unstyled mr-n3 flex-wrap mr-n8 justify-content-md-end">
+                                  <li className="mt-2 mr-8 font-size-small text-black-2 d-flex">
+                                    <span
+                                      className="mr-4"
+                                      css={`
+                                        margin-top: -2px;
+                                      `}
+                                    >
+                                      <img src={iconL} alt="" />
+                                    </span>
+                                    <span className="font-weight-semibold">
+                                      {
+                                        profesional.persona.persona_direccion[0]
+                                          .ciudad.nombre
+                                      }
+                                    </span>
+                                  </li>
+                                  {/* <li className="mt-2 mr-8 font-size-small text-black-2 d-flex">
                                   <span
                                     className="mr-4"
                                     css={`
@@ -281,30 +284,32 @@ const SearchGrid = () => {
                                     9d ago
                                   </span>
                                 </li> */}
-                              </ul>
-                            </div>
-                            <div className="col-md-12 d-flex justify-content-end pr-0">
-                              <a
-                                className="btn btn-transparent text-uppercase font-size-3 heading-default-color focus-reset text-primary"
-                                href={"/candidate-profile?id=" + profesional.id}
-                              >
-                                Ver perfil
-                              </a>
+                                </ul>
+                              </div>
+                              <div className="col-md-12 d-flex justify-content-end pr-0">
+                                <a
+                                  className="btn btn-transparent text-uppercase font-size-3 heading-default-color focus-reset text-primary"
+                                  href={
+                                    "/candidate-profile?id=" + profesional.id
+                                  }
+                                >
+                                  Ver perfil
+                                </a>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  })}
-                  <div className="text-center pt-5 pt-lg-13">
-                    <Link href="/#">
-                      <a className="text-green font-weight-bold text-uppercase font-size-3">
-                        Cargar más <i className="fas fa-sort-down ml-3"></i>
-                      </a>
-                    </Link>
+                      );
+                    })}
+                    <div className="text-center pt-5 pt-lg-13">
+                      <Link href="/#">
+                        <a className="text-green font-weight-bold text-uppercase font-size-3">
+                          Cargar más <i className="fas fa-sort-down ml-3"></i>
+                        </a>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-                {/* <!-- form end --> */}
+                )}
               </div>
             </div>
           </div>
