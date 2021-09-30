336460e33d6ec7556e4d8c6cd2f60627eb6cf83f
acem0301 <acem0301@gmail.com>
2021-09-30 01:38:31 -0400

    Fix

--- src/services/profesional.service.js
+++ src/services/profesional.service.js
@@ -3,7 +3,7 @@ import http from "./http-common";
 class ProfesionalService {
   getProfile = async (id) => {
     try {
-      const data = await http.get("/public/usuario/profesional/perfil/1");
+      const data = await http.get("/public/usuario/profesional/perfil/" + id);
       return data;
     } catch (error) {
       console.log(error);
