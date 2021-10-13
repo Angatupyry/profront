110f9cf3febc94300daf67beb27499b2c3665393
acem0301 <acem0301@gmail.com>
2021-10-13 01:05:02 -0300

    Integración post tarjeta

--- src/components/ModalAddPayment/ModalAddPayment.js
+++ src/components/ModalAddPayment/ModalAddPayment.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Router from "next/router";
 import styled from "styled-components";
 import { Modal } from "react-bootstrap";
@@ -7,7 +7,9 @@ import AuthService from "../../services/auth.service";
 import Error from "../Error/Error";
 import { Select } from "../../components/Core";
 
-const tarjetas = [{ value: "C", label: "Crédito" }];
+import MediosPagoService from "../../services/mediosPago.service";
+
+//const tarjetas = [{ value: "C", label: "Crédito" }];
 
 const ModalStyled = styled(Modal)`
   /* &.modal {
@@ -15,43 +17,163 @@ const ModalStyled = styled(Modal)`
   } */
 `;
 
+const cardArray = [];
+
+const meses = [
+  { value: "1", label: "Enero" },
+  { value: "2", label: "Febrero" },
+  { value: "3", label: "Marzo" },
+  { value: "4", label: "Abril" },
+  { value: "5", label: "Mayo" },
+  { value: "6", label: "Junio" },
+  { value: "7", label: "Julio" },
+  { value: "8", label: "Agosto" },
+  { value: "9", label: "Setiembre" },
+  { value: "10", label: "Octubre" },
+  { value: "11", label: "Noviembre" },
+  { value: "12", label: "Diciembre" },
+];
+
+const anhos = [
+  { value: "2021", label: "2021" },
+  { value: "2022", label: "2022" },
+  { value: "2023", label: "2023" },
+  { value: "2024", label: "2024" },
+  { value: "2025", label: "2025" },
+  { value: "2026", label: "2026" },
+  { value: "2027", label: "2027" },
+  { value: "2028", label: "2028" },
+  { value: "2029", label: "2029" },
+  { value: "2030", label: "2030" },
+];
+
+const getTodaysdate = (e) => {
+  var today = new Date();
+  return (
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+  );
+};
+
 const ModalAddPayment = (props) => {
-  const [showPass, setShowPass] = useState(true);
+  const [tarjetas, setTarjetas] = React.useState([]);
   const gContext = useContext(GlobalContext);
   const [state, setState] = React.useState({
     loading: true,
     error: null,
-    email: "",
-    password: "",
+    success: false,
+    card: "",
+    cardLabel: "",
+  });
+
+  const [postData, setPostData] = React.useState({
+    descripcion: "",
+    tipo_tarjeta_id: 0,
+    usuario_id: 1,
+    numero_tarjeta: "",
+    fecha_vencimiento_tarjeta: getTodaysdate(),
+    numero_cvc: "",
+    secuencia: 1,
+    mes_vencimiento: meses[0].value,
+    mes_vencimiento_label: meses[0].label,
+    anho_vencimiento: anhos[0].value,
+    anho_vencimiento_label: anhos[0].label,
   });
 
   const handleClose = () => {
     gContext.toggleAddPaymentModal();
   };
 
-  const togglePassword = () => {
-    setShowPass(!showPass);
+  const handleChange = (e) => {
+    const newPostData = { ...postData };
+    newPostData[e.target.id] = e.target.value;
+    setPostData(newPostData);
   };
+
+  function isEmpty(value) {
+    return (
+      Boolean(value && typeof value === "object") && !Object.keys(value).length
+    );
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  async function getCards() {
+    setState({ loading: true, error: null });
+    try {
+      const cards = await MediosPagoService.getCards();
+      console.log(cards);
+      cards.data.data.forEach((element) => {
+        cardArray.push({
+          value: element.id.toString(),
+          label: element.nombre,
+        });
+      });
+      setTarjetas(cardArray);
+      setState({
+        card: cardArray[0].value,
+        cardLabel: cardArray[0].label,
+      });
+
+      setPostData({
+        tipo_tarjeta_id: parseInt(cardArray[0].value),
+        mes_vencimiento: meses[0].value,
+        mes_vencimiento_label: meses[0].label,
+        anho_vencimiento: anhos[0].value,
+        anho_vencimiento_label: anhos[0].label,
+        usuario_id: 1,
+        secuencia: 1,
+        fecha_vencimiento_tarjeta: getTodaysdate(),
+      });
+    } catch (error) {
+      console.log(error);
+      setState({ loading: false, error: error });
+    }
+  }
+
+  useEffect(() => {
+    if (!tarjetas.length > 0) {
+      getCards();
+    }
+    scrollToTop();
+  }, [tarjetas]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setState({ loading: true, error: null });
-
     try {
-      await AuthService.login(state.email, state.password);
-      setState({ loading: false, error: null });
-      handleClose();
-      Router.push("/dashboard-main");
+      const response = await MediosPagoService.postCard(
+        postData.descripcion,
+        postData.tipo_tarjeta_id,
+        postData.usuario_id,
+        postData.numero_tarjeta,
+        postData.fecha_vencimiento_tarjeta,
+        postData.numero_cvc,
+        postData.secuencia
+      );
+      setState({ loading: false, error: null, success: true });
     } catch (error) {
       console.log(error);
       setState({ loading: false, error: error });
     }
   };
 
-  const handleChange = (e) => {
-    const newState = { ...state };
-    newState[e.target.id] = e.target.value;
-    setState(newState);
+  const handleMesVencimiento = (e) => {
+    const newPostData = { ...postData };
+    newPostData["mes_vencimiento"] = e.value;
+    newPostData["mes_vencimiento_label"] = e.label;
+    setPostData(newPostData);
+  };
+
+  const handleAnhoVencimiento = (e) => {
+    const newPostData = { ...postData };
+    newPostData["anho_vencimiento"] = e.value;
+    newPostData["anho_vencimiento_label"] = e.label;
+    setPostData(newPostData);
   };
 
   return (
@@ -88,10 +210,10 @@ const ModalAddPayment = (props) => {
               </div>
               <div className="col-lg-7 col-md-6">
                 <div className="bg-white-2 h-100 px-11 pt-11 pb-7">
-                  <form action="/">
+                  <form action="/" onSubmit={handleSubmit}>
                     <div className="form-group">
                       <label
-                        htmlFor="description"
+                        htmlFor="descripcion"
                         className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
                       >
                         Descripción
@@ -100,9 +222,9 @@ const ModalAddPayment = (props) => {
                         type="text"
                         className="form-control"
                         placeholder="Mi tarjeta visa"
-                        id="description"
-                        // value={state.email}
-                        // onChange={handleChange}
+                        id="descripcion"
+                        value={postData.descripcion}
+                        onChange={handleChange}
                       />
                     </div>
 
@@ -117,15 +239,18 @@ const ModalAddPayment = (props) => {
                         options={tarjetas}
                         className="form-control pl-0 arrow-3 w-100 font-size-4 d-flex align-items-center w-100 "
                         border={false}
-                        id="select2"
-                        // value={{ label: state.sexLabel, value: state.sex }}
+                        id="tipoTarjeta"
+                        value={{
+                          label: state.cardLabel,
+                          value: state.card,
+                        }}
                         // onChange={handleSelect}
                       />
                     </div>
 
                     <div className="form-group">
                       <label
-                        htmlFor="description"
+                        htmlFor="numero_tarjeta"
                         className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
                       >
                         Número de tarjeta
@@ -134,15 +259,15 @@ const ModalAddPayment = (props) => {
                         type="text"
                         className="form-control"
                         placeholder="0214 1544 7845 4445"
-                        id="description"
-                        // value={state.email}
-                        // onChange={handleChange}
+                        id="numero_tarjeta"
+                        value={postData.numero_tarjeta}
+                        onChange={handleChange}
                       />
                     </div>
 
                     <div className="form-group">
                       <label
-                        htmlFor="description"
+                        htmlFor="nombre_titular"
                         className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
                       >
                         Nombre del titular
@@ -151,32 +276,60 @@ const ModalAddPayment = (props) => {
                         type="text"
                         className="form-control"
                         placeholder="Pedro Gimenez"
-                        id="description"
-                        // value={state.email}
+                        id="nombre_titular"
+                        // value={postData.no}
                         // onChange={handleChange}
                       />
+                    </div>
+                    <div className="row no-gutters">
+                      <div className="col-lg-6 col-md-6 p-0 pr-2">
+                        <div className="form-group">
+                          <label
+                            htmlFor="select2"
+                            className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
+                          >
+                            Mes vencimiento
+                          </label>
+                          <Select
+                            options={meses}
+                            className="form-control pl-0 arrow-3 w-100 font-size-4 d-flex align-items-center w-100 "
+                            border={false}
+                            id="tipoTarjeta"
+                            value={{
+                              label: postData.mes_vencimiento_label,
+                              value: postData.mes_vencimiento,
+                            }}
+                            onChange={handleMesVencimiento}
+                          />
+                        </div>{" "}
+                      </div>
+
+                      <div className="col-lg-6 col-md-6 pr-0 pl-2">
+                        <div className="form-group">
+                          <label
+                            htmlFor="select2"
+                            className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
+                          >
+                            Año vencimiento
+                          </label>
+                          <Select
+                            options={anhos}
+                            className="form-control pl-0 arrow-3 w-100 font-size-4 d-flex align-items-center w-100 "
+                            border={false}
+                            id="tipoTarjeta"
+                            value={{
+                              label: postData.anho_vencimiento_label,
+                              value: postData.anho_vencimiento,
+                            }}
+                            onChange={handleAnhoVencimiento}
+                          />
+                        </div>{" "}
+                      </div>
                     </div>
 
                     <div className="form-group">
                       <label
-                        htmlFor="description"
-                        className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
-                      >
-                        Fecha de vencimiento
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="12/24"
-                        id="description"
-                        // value={state.email}
-                        // onChange={handleChange}
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label
-                        htmlFor="description"
+                        htmlFor="numero_cvc"
                         className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
                       >
                         CVV
@@ -185,15 +338,15 @@ const ModalAddPayment = (props) => {
                         type="text"
                         className="form-control"
                         placeholder="123"
-                        id="description"
-                        // value={state.email}
-                        // onChange={handleChange}
+                        id="numero_cvc"
+                        value={postData.numero_cvc}
+                        onChange={handleChange}
                       />
                     </div>
 
                     <div className="form-group">
                       <label
-                        htmlFor="description"
+                        htmlFor="datos_adicionales"
                         className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
                       >
                         Datos adicionales
@@ -202,7 +355,7 @@ const ModalAddPayment = (props) => {
                         type="text"
                         className="form-control"
                         placeholder="54846"
-                        id="description"
+                        id="datos_adicionales"
                         // value={state.email}
                         // onChange={handleChange}
                       />
@@ -216,6 +369,11 @@ const ModalAddPayment = (props) => {
                         Guardar{" "}
                       </button>
                     </div>
+                    {state.success && (
+                      <div class="alert alert-success" role="alert">
+                        Tarjeta guardada exitosamente.
+                      </div>
+                    )}
                   </form>
                 </div>
               </div>
