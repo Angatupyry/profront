e9f170bca711d3f9db2996e8e6bf40226a755de4
acem0301 <acem0301@gmail.com>
2021-11-18 18:19:19 -0300

    Integración mi perfil/servicios

--- src/components/ModalSignUp/ModalSignUp.js
+++ src/components/ModalSignUp/ModalSignUp.js
@@ -118,8 +118,8 @@ const ModalSignUp = (props) => {
       );
       setState({ loading: false, error: null, success: true });
       console.log(response);
-      Cookies.set("token", response.data.token);
-      Cookies.set("user", response.data.user);
+      Cookies.set("token", JSON.stringify(response.data.token));
+      Cookies.set("user", JSON.stringify(response.data.user));
       //console.log(state);
       setTimeout(() => {
         handleClose();
